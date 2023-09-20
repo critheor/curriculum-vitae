15b65f1ca88d31771a3f52d5f0c48553b0c05f72
critheor <mateoherreraramirez98@gmail.com>
2023-09-19 22:45:33 -0500

    Ajustar estilos al componente

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import "./style.css";
 
 export default function Header() {
   const [expand, setExpand] = useState(false);
@@ -14,16 +15,29 @@ export default function Header() {
           expand="md"
           className="d-flex aling-items-center justify-content-between"
           onToggle={handleExpand}
+          collapseOnSelect
         >
-          <Navbar.Brand>Mateo Herrera</Navbar.Brand>
-          <Navbar.Toggle aria-controls="movile-nav">Menu</Navbar.Toggle>
+          <Navbar.Brand className="text-color">Mateo Herrera</Navbar.Brand>
+          <Navbar.Toggle aria-controls="movile-nav" className="text-color">
+            Menu
+          </Navbar.Toggle>
           <Navbar.Collapse id="movile-nav" className="flex-grow-0">
             <Nav>
-              <Nav.Link>Home</Nav.Link>
-              <Nav.Link>About</Nav.Link>
-              <Nav.Link>Resume</Nav.Link>
-              <Nav.Link>Blog</Nav.Link>
-              <Nav.Link>Contact</Nav.Link>
+              <Nav.Item>
+                <Nav.Link href='#home' style={{color: '#fff'}}>Home</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href='#about' style={{color: '#fff'}}>About</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href='#resume' style={{color: '#fff'}}>Resume</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href='#blog' style={{color: '#fff'}}>Blog</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href='#contact' style={{color: '#fff'}}>Contact</Nav.Link>
+              </Nav.Item>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
