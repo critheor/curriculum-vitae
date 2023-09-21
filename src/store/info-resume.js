85ec649fc519c35c3be6de4704ddc5cba7d0437f
critheor <mateoherreraramirez98@gmail.com>
2023-09-21 15:15:03 -0500

    Datos para almacenar informacion acerca del perfil laboral

---
+++ src/store/info-resume.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        title: 'Work Experience',
+        dataResume: [
+            {
+                range: '2023 - actual',
+                subtitle: 'soporte y desarrollador',
+                description: 'descripcion del trabajoooo....'
+            },
+        ]
+    },
+    {
+        title: 'Education',
+        dataResume: [
+            {
+                range: '2018 - actual',
+                subtitle: 'Universidad Distrital',
+                description: 'tecnologia en sistematizacion de datos'
+            }
+        ]
+    },
+    {
+        title: 'Skills & Experience',
+        dataResume: [
+            {
+                range: '',
+                subtitle: "React Js",
+                description: 'Pagina web desarrollada con esta tecnologia'
+            }
+        ]
+    }
+]
