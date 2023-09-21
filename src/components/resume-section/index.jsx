5ad1f8f3c9000501e6cef2ee7867ef9298b15bf8
critheor <mateoherreraramirez98@gmail.com>
2023-09-21 15:12:09 -0500

    Ajustar el componente, para que muestre los datos necesarios

--- src/components/resume-section/index.jsx
+++ src/components/resume-section/index.jsx
@@ -1,18 +1,24 @@
-import { Col, Container, Row } from 'react-bootstrap'
-import InfoResumeCard from '../info-resume-card'
-import './style.css'
+import { Col, Container, Row } from "react-bootstrap";
+import InfoResumeCard from "../info-resume-card";
+import "./style.css";
 
-export default function ResumeSection({title}) {
+export default function ResumeSection({ title, data }) {
   return (
-    <Container className='resume-s-pt'>
-        <Row>
-            <Col sm={4}>
-                <h4>{title || 'Default Title'}</h4>
-            </Col>
-            <Col sm={8}>
-                <InfoResumeCard />
-            </Col>
-        </Row>
+    <Container className="resume-s-pt">
+      <Row>
+        <Col sm={4}>
+          <h4 className="fs-title">{title || "Default Title"}</h4>
+        </Col>
+        <Col sm={8}>
+          {data.map((item) => (
+            <InfoResumeCard
+              range={item.range}
+              subtitle={item.subtitle}
+              description={item.description}
+            />
+          ))}
+        </Col>
+      </Row>
     </Container>
-  )
+  );
 }
