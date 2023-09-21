36fb7416168ab81cef7d1f519d8c807f18194d28
critheor <mateoherreraramirez98@gmail.com>
2023-09-21 15:15:59 -0500

    Componente inicial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
-import 'normalize.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "normalize.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/header";
+import Hero from "./components/hero";
+import About from "./components/about";
+import Resume from "./components/resume";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Hero />
+      <About />
+      <Resume />
     </div>
   );
 }
