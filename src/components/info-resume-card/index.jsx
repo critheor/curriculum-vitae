f40e9776f0f4f88634b7ec371b8778f095f09537
critheor <mateoherreraramirez98@gmail.com>
2023-09-21 15:11:09 -0500

    Ajustar el componente, para mostrar los datos adecuados

--- src/components/info-resume-card/index.jsx
+++ src/components/info-resume-card/index.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import { Card, Col, Row } from "react-bootstrap";
 import "./style.css";
 
-export default function InfoResumeCard({ range, role, description }) {
+export default function InfoResumeCard({ range, subtitle, description }) {
   return (
-    <Card className="border-white mb-3">
+    <Card className="info-resume-card border-white mb-3">
       <Row>
         <Col sm={4}>
-          <Card.Title className="fw-lighter fs-0">{range || "2023 - actual"}</Card.Title>
+          <Card.Title className="fw-lighter fs-1">{range}</Card.Title>
         </Col>
         <Col sm={8}>
-          <Card.Title className="fw-bold fs-0">{role || "XGenCode"}</Card.Title>
+          <Card.Title className="fw-bold fs-0">{subtitle || "XGenCode"}</Card.Title>
           <Card.Text>
             {description || 'descripcion...'}
           </Card.Text>
