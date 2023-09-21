406226b0e7979f3e07d375ae06bd24ff20240c12
critheor <mateoherreraramirez98@gmail.com>
2023-09-21 15:13:16 -0500

    Ajustar el componente, para mostrar la seccion que muestra el Resumen

--- src/components/resume/index.jsx
+++ src/components/resume/index.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import ResumeSection from "../resume-section";
+import dataResume from "../../store/info-resume.js";
 import "./style.css";
 
 export default function Resume() {
   return (
     <div className="resume">
       <Container>
-        <h3 className="title-r fw-bold">
-          Resume
-        </h3>
-        <ResumeSection title={'Example'}/>
+        <h3 className="title-r fw-bold">Resume</h3>
+        {dataResume.map((item) => (
+          <ResumeSection title={item.title} data={item.dataResume} />
+        ))}
       </Container>
     </div>
   );
